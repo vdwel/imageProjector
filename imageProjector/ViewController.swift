9eb603e50aaf22d06bd391cca00a6c334ad51242
Sander <sander@van-der-wel.net>
2016-07-26 11:44:04 +0200

    pixelscanning

--- imageProjector/ViewController.swift
+++ imageProjector/ViewController.swift
@@ -8,7 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+extension UIImage {
+    func getPixelColor(pos: CGPoint) -> UIColor {
+        
+        let pixelData = CGDataProviderCopyData(CGImageGetDataProvider(self.CGImage))
+        let data: UnsafePointer<UInt8> = CFDataGetBytePtr(pixelData)
+        
+        let pixelInfo: Int = ((Int(self.size.width) * Int(pos.y)) + Int(pos.x)) * 4
+        
+        let r = CGFloat(data[pixelInfo]) / CGFloat(255.0)
+        let g = CGFloat(data[pixelInfo+1]) / CGFloat(255.0)
+        let b = CGFloat(data[pixelInfo+2]) / CGFloat(255.0)
+        let a = CGFloat(data[pixelInfo+3]) / CGFloat(255.0)
+        
+        return UIColor(red: r, green: g, blue: b, alpha: a)
+    }  
+}
+
+extension UIColor {
+    func rgb() -> (red:Int, green:Int, blue:Int, alpha:Int)? {
+        var fRed : CGFloat = 0
+        var fGreen : CGFloat = 0
+        var fBlue : CGFloat = 0
+        var fAlpha: CGFloat = 0
+        if self.getRed(&fRed, green: &fGreen, blue: &fBlue, alpha: &fAlpha) {
+            let iRed = Int(fRed * 255.0)
+            let iGreen = Int(fGreen * 255.0)
+            let iBlue = Int(fBlue * 255.0)
+            let iAlpha = Int(fAlpha * 255.0)
+            
+            return (red:iRed, green:iGreen, blue:iBlue, alpha:iAlpha)
+        } else {
+            // Could not extract RGBA components:
+            return nil
+        }
+    }
+}
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +57,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var imagePicked: UIImageView!
 
+    @IBAction func buttonOpenImage(sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary;
+            imagePicker.allowsEditing = true
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    
+    
+    @IBAction func buttonProjectImage(sender: UIButton) {
+        let VWIDTH:CGFloat = 200
+        let VHEIGHT:CGFloat = 144
+        let height = imagePicked.image?.size.height
+        let width = imagePicked.image?.size.width
+        NSLog("Image Size: (%d, %d)",Int(width!) ,Int(height!))
+        for x in 0...199 {
+            for y in 0...143 {
+                let color : UIColor = imagePicked.image!.getPixelColor(CGPointMake(CGFloat(x) * width!/VWIDTH, CGFloat(y) * height!/VHEIGHT))
+                let rgb = color.rgb()
+                //NSLog("pixel (%d,%d): %d, %d, %d, %d", x, y, rgb!.red, rgb!.green, rgb!.blue, rgb!.alpha)
+            }
+        }
+    }
+    
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        imagePicked.image = image
+        self.dismissViewControllerAnimated(true, completion: nil);
+    }
 }
 
